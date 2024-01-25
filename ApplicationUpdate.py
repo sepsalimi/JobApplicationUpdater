119849479531d24f89caae32115493a0ee71ed6e
Sepehr Salimi <sepehr.h.salimi@gmail.com>
2024-01-25 00:36:53 -0500

    Adjusts file name if too long for Windows

--- ApplicationUpdate.py
+++ ApplicationUpdate.py
@@ -4,6 +4,7 @@ from PyPDF2 import PdfMerger
 from datetime import datetime
 import subprocess
 import comtypes.client # word to PDF
+import shutil
 
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
@@ -13,7 +14,8 @@ from selenium.webdriver.edge.service import Service
 
 import time
 
-
+# Input your name
+Name = 'Sepehr Salimi'
 # Input parent directory path
 directory_path = r'\\SepehrNAS\Thick Volume\CAREER\SEPEHR\Job Related\Resume & Cover Letter\APPLICATIONS\2024'
 # Specify the path to Edge WebDriver executable
@@ -42,6 +44,15 @@ def merge_pdfs(pdf_list, output):
     merger.write(output)
     merger.close()
     
+# Reduces file name size if required
+def create_shortened_paths_if_needed():
+    global cover_letter_docx, final_coverletter, final_application_pdf, final_resume_pdf
+    if len(cover_letter_file_path) > 83:
+        cover_letter_docx = cover_letter_docx_shortened
+        final_coverletter = final_coverletter_shortened
+        final_application_pdf = final_application_pdf_shortened
+        final_resume_pdf = final_resume_pdf_shortened    
+   
 def screenshot_webpage(url, save_path):
     driver = webdriver.Edge() 
     driver.get(url)
@@ -78,37 +89,61 @@ def save_webpage_as_html(url, save_path):
 # User Input
 # url = input("Enter job posting url: ")
 company = input("Enter the company name: ")
-position = input("Enter the position: ")
+job_title = input("Enter the RE: Position: ")
+role = input("... excitement that I submit my application for the ____ position: ")
 skill_role = input("Join company..., further advancing my skills in: ")
 
 # Get current date
-current_date = datetime.now().strftime("%B %d, %Y")
+CurrentDateCoverLetter = datetime.now().strftime("%B %d, %Y")
+CurrentDateFileName = datetime.now().strftime("%Y%m%d")
 
 # Create new folder for the application
-company_folder = os.path.join(directory_path, company, f'{position} - {current_date}')
+company_folder = os.path.join(directory_path, "Applications Sent Out", company, f'{job_title} - {CurrentDateFileName}')
 os.makedirs(company_folder, exist_ok=True)
 
+cover_letter_file_path = f'{Name} - Cover Letter - {company} - {job_title}.pdf' # used to check if file name length is appropriate
+
 # Define file paths
-cover_letter_template = os.path.join(directory_path, 'Sepehr Salimi - Cover Letter.docx')
-temp_docx = os.path.join(company_folder, f'Sepehr Salimi - Cover Letter - {company} - {position}.docx')
-temp_pdf = os.path.join(company_folder, f'Sepehr Salimi - Cover Letter - {company} - {position}.pdf')
-final_pdf = os.path.join(company_folder, f'Sepehr Salimi - Application - {company} - {position}.pdf')
-resume_pdf = os.path.join(directory_path, 'Sepehr Salimi - Resume.pdf')
-final_png = os.path.join(company_folder, f'Job Posting - {company} - {position}.png')
-final_html = os.path.join(company_folder, f'Job Posting - {company} - {position}.html')
+cover_letter_template = os.path.join(directory_path, f'{Name} - Cover Letter.docx')
+cover_letter_docx = os.path.join(company_folder, f'{Name} - Cover Letter - {company} - {job_title}.docx')
+resume_pdf = os.path.join(directory_path, f'{Name} - Resume.pdf')
+
+final_coverletter = os.path.join(company_folder, cover_letter_file_path) 
+
+final_application_pdf = os.path.join(company_folder, f'{Name} - Application - {company} - {job_title}.pdf')
+final_resume_pdf = os.path.join(company_folder, f'{Name} - Resume - {company} - {job_title}.pdf')
+final_png = os.path.join(company_folder, f'Job Posting - {company} - {job_title}.png')
+final_html = os.path.join(company_folder, f'Job Posting - {company} - {job_title}.html')
+
+cover_letter_docx_shortened  = os.path.join(company_folder, f'{Name} - Cover Letter - {company} - {role}.docx')
+final_coverletter_shortened = os.path.join(company_folder, f'{Name} - Cover Letter - {company} - {role}.pdf')
+final_application_pdf_shortened = os.path.join(company_folder, f'{Name} - Application - {company} - {role}.pdf')
+final_resume_pdf_shortened = os.path.join(company_folder, f'{Name} - Resume - {company} - {role}.pdf')
+final_png = os.path.join(company_folder, f'Job Posting - {company} - {role}.png')
+final_html = os.path.join(company_folder, f'Job Posting - {company} - {role}.html')
 
 
 # Process Document
 doc = Document(cover_letter_template)
 replace_text_in_doc(doc, "COMPANY", company)
-replace_text_in_doc(doc, "POSITION", position)
-replace_text_in_doc(doc, "DATE", current_date)
+replace_text_in_doc(doc, "JOBTITLE", job_title)
+replace_text_in_doc(doc, "POSITION", role)
+replace_text_in_doc(doc, "DATE", CurrentDateCoverLetter)
 replace_text_in_doc(doc, "SKILL", skill_role)
-doc.save(temp_docx)
+
+
+# Adjust paths if needed before assigning file names
+create_shortened_paths_if_needed()
+
+# Copy the resume PDF to the new location with the new name
+shutil.copy(resume_pdf, final_resume_pdf)
+
+# save new .docx cover letter
+doc.save(cover_letter_docx)
 
 # Convert to PDF and merge
-save_as_pdf(temp_docx, temp_pdf)
-merge_pdfs([temp_pdf, resume_pdf], final_pdf)
+save_as_pdf(cover_letter_docx, final_coverletter)
+merge_pdfs([final_coverletter, resume_pdf], final_application_pdf)
 
 # # Screnshot job posting
 # screenshot_webpage(url, final_png)
